45e45e749d94fc8b1bec3f202c59c573290c0170
Максим Алексеев <mrvalter566@gmail.com>
2020-08-16 19:41:52 +0700

    Bug fix

--- model/operation.go
+++ model/operation.go
@@ -5,7 +5,7 @@ import "time"
 // Operation structure
 type Operation struct {
 	Title  string    `json:"title,omitempty" bson:"title,omitempty"`
-	Amount int32     `json:"amount,omitempty" bson:"amount,omitempty"`
+	Amount int       `json:"amount,omitempty" bson:"amount,omitempty"`
 	Type   string    `json:"type,omitempty" bson:"type,omitempty"`
 	Time   time.Time `json:"time,omitempty" bson:"time,omitempty"`
 	ID     int       `json:"id,omitempty" bson:"id,omitempty"`
