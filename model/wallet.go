fa36715cd4760fd38a21a9ecface46e533700e76
Максим Алексеев <mrvalter566@gmail.com>
2020-08-29 20:54:13 +0700

    Add changes to wallet's model

--- model/wallet.go
+++ model/wallet.go
@@ -4,8 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Wallet ...
 type Wallet struct {
-	Title      string   `json:"title,omitempty" bson:"title,omitempty"`
-	Colors     []string `json:"colors,omitempty" bson:"colors,omitempty"`
-	Categories []Category
+	Title      string             `json:"title,omitempty" bson:"title,omitempty"`
+	Colors     []string           `json:"colors,omitempty" bson:"colors,omitempty"`
+	Categories []Category         `json:"categories,omitempty" bson:"categories,omitempty"`
 	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 }
