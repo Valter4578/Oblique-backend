df98b8f60441ff17c7ed95ed8c9d7a610c7c48ff
Максим Алексеев <mrvalter566@gmail.com>
2020-08-29 23:41:33 +0700

    Add some changes

--- database/category.go
+++ database/category.go
@@ -6,6 +6,7 @@ import (
 	"oblique/model"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -23,4 +24,20 @@ func InsertCategory(category *model.Category) *mongo.InsertOneResult {
 	}
 
 	return result
+}
+
+func GetCategory(id primitive.ObjectID, category *model.Category) *error {
+	log.Println("Database: GetCategory")
+
+	collection := client.Database("oblique-dev").Collection("categories")
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	err := collection.FindOne(ctx, model.Category{ID: id}).Decode(&category)
+	if err != nil {
+		log.Println(err)
+		return &err
+	}
+
+	return nil
 }
