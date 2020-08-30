d4d422fcbca43ba324dc9a0db5569838bb63ba73
Максим Алексеев <mrvalter566@gmail.com>
2020-08-30 21:46:21 +0700

    Refactor in database/wallet.go

--- database/wallet.go
+++ database/wallet.go
@@ -29,23 +29,25 @@ func InsertWallet(wallet *model.Wallet) *mongo.InsertOneResult {
 }
 
 // GetWallet gets id of wallet and pointer to wallet for decode result into it
-func GetWallet(id primitive.ObjectID, wallet *model.Wallet) *error {
+func GetWallet(id primitive.ObjectID) (*error, *model.Wallet) {
 	log.Println("Database: GetWallet")
 
 	collection := client.Database("oblique-dev").Collection("wallets")
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
+	var wallet *model.Wallet
 	err := collection.FindOne(ctx, model.Wallet{ID: id}).Decode(&wallet)
 	if err != nil {
 		log.Println(err)
-		return &err
+		return &err, nil
 	}
 
-	return nil
+	return nil, wallet
 }
 
-func GetWallets(wallets *[]model.Wallet) *error {
+// GetWallets fetch all wallets from db and returns them and error
+func GetWallets() (*error, *[]model.Wallet) {
 	log.Println("Database: GetWallets")
 
 	collection := client.Database("oblique-dev").Collection("wallets")
@@ -55,16 +57,17 @@ func GetWallets(wallets *[]model.Wallet) *error {
 	cursor, err := collection.Find(ctx, bson.D{})
 	if err != nil {
 		log.Println(err)
-		return &err
+		return &err, nil
 	}
 	defer cursor.Close(ctx)
 
+	var wallets *[]model.Wallet
 	for cursor.Next(ctx) {
 		var wallet model.Wallet
 		err = cursor.Decode(&wallet)
 		if err != nil {
 			log.Println(err)
-			return &err
+			return &err, nil
 		}
 
 		*wallets = append(*wallets, wallet)
@@ -73,12 +76,13 @@ func GetWallets(wallets *[]model.Wallet) *error {
 	err = cursor.Err()
 	if err != nil {
 		log.Println(err)
-		return &err
+		return &err, nil
 	}
 
-	return nil
+	return nil, wallets
 }
 
+// UpdateWallet gets the id and bson object that contains the updates fields. Returns the *mongo.UpdateResult
 func UpdateWallet(id primitive.ObjectID, update bson.D) *mongo.UpdateResult {
 	log.Println("Database: UpdateWallet")
 
