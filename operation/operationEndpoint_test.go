eb432ea38566acc4e6cb4ea654fbfce828750541
Valter4578 <mrvalter566@gmail.com>
2020-09-12 22:28:46 +0700

    Add test fot GetOperation

--- operation/operationEndpoint_test.go
+++ operation/operationEndpoint_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
 )
 
@@ -33,6 +34,33 @@ func TestGetOperations(t *testing.T) {
 	expected := `[{"title":"post test mongo operation 1","amount":123431,"time":"0001-01-01T00:00:00Z","_id":"5f54fdb89541a3b42f61ef83"},{"title":"test mongo insert operation to category 1","amount":1231323,"time":"0001-01-01T00:00:00Z","_id":"5f5b7d45263b0499a79f6da9"},{"title":"test mongo insert operation to category 2","amount":1231323,"time":"0001-01-01T00:00:00Z","_id":"5f5b7d91263b0499a79f6daa"},{"title":"test mongo insert operation to category 3","amount":1231323,"time":"0001-01-01T00:00:00Z","_id":"5f5b7de23523ab969fbf9207"},{"title":"test mongo insert operation to category 3","amount":1231323,"time":"0001-01-01T00:00:00Z","_id":"5f5b7dea3523ab969fbf9208"}]`
 	body := rr.Body.String()
 
+	require.JSONEq(t, expected, body)
+}
+
+func TestGetOperation(t *testing.T) {
+	uri := os.Getenv("URI")
+	fmt.Println(uri)
+	db.ConnectDB(&uri)
+
+	req, err := http.NewRequest("GET", `/operation/`, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	vars := map[string]string{"id": "5f54fdb89541a3b42f61ef83"}
+	req = mux.SetURLVars(req, vars)
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetOperation)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"title":"post test mongo operation 1","amount":123431,"time":"0001-01-01T00:00:00Z","_id":"5f54fdb89541a3b42f61ef83"}`
+	body := rr.Body.String()
 
 	require.JSONEq(t, expected, body)
 }
