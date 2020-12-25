ed6182cd7d484e70e9f6b65683d72ea1fb5b6c72
Valter4578 <mrvalter566@gmail.com>
2020-12-25 20:37:17 +0700

    Add new field for wallet - amount

--- iternal/app/model/wallet.go
+++ iternal/app/model/wallet.go
@@ -7,5 +7,6 @@ type Wallet struct {
 	Title      string             `json:"title,omitempty" bson:"title,omitempty"`
 	Colors     []string           `json:"colors,omitempty" bson:"colors,omitempty"`
 	Categories []Category         `json:"categories,omitempty" bson:"categories,omitempty"`
+	Amount     int64              `json:"amount,omitempty" bson:amount,omitempty`
 	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 }
